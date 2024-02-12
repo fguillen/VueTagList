8d11ce23bf63506a8a6f090a108e378673a172c9
Fernando Guillen <fguillen.mail@gmail.com>
2024-02-12 21:24:01 +0700

    Tag Suggestions work

--- src/components/AddTag.vue
+++ src/components/AddTag.vue
@@ -1,9 +1,9 @@
 <template>
   <form v-on:submit.prevent="addTag">
-    <input type="text" v-model.trim="title" />
+    <input type="text" v-model.trim="title" @keydown="onKeyDown"/>
     <button type="submit">+</button>
   </form>
-  <TagsSuggestion :partial-title="title" />
+  <TagsSuggestion :partial-title="title" ref="tagsSuggestionRef" @suggestion-selected="onSuggestionSelected"/>
 </template>
 
 <script setup lang="ts">
@@ -12,6 +12,7 @@ import { useTagsStore } from '@/stores/TagsStore';
 import TagsSuggestion from './TagsSuggestion.vue';
 
 const title: Ref<string> = ref("");
+const tagsSuggestionRef = ref(null);
 
 const { addTag: storeAddTag } = useTagsStore();
 
@@ -20,5 +21,16 @@ const addTag = () => {
   console.log(title.value);
   storeAddTag(title.value);
   title.value = "";
+}
+
+const onKeyDown = (event: KeyboardEvent): void => {
+  console.log("AddTag.onKeyDown", event.code)
+  if (tagsSuggestionRef.value != undefined) {
+    tagsSuggestionRef.value.onKeyDown(event.code);
+  }
+}
+
+const onSuggestionSelected = (suggestion: string): void => {
+  title.value = suggestion;
 }
 </script>
--- src/components/TagsSuggestion.vue
+++ src/components/TagsSuggestion.vue
@@ -8,12 +8,13 @@
 
 <script setup lang="ts">
 import { ref, watch, type Ref } from 'vue';
-
 import { useSuggestionsFinder } from '@/stores/SuggestionsFinder'
 
 const props = defineProps<{
   partialTitle: string
 }>();
+
+const emit = defineEmits(["suggestionSelected"]);
 
 const { getSuggestions } = useSuggestionsFinder();
 
@@ -32,4 +33,59 @@ watch(() => props.partialTitle, (): void => {
   }
 });
 
+const selectNext = ():void => {
+  if (suggestedTags.value.length > 0) {
+    const index = suggestedTags.value.findIndex(e => e.selected);
+    const nextIndex = (index + 1) % suggestedTags.value.length;
+
+    if (index != -1) {
+      suggestedTags.value[index].selected = false;
+    }
+    suggestedTags.value[nextIndex].selected = true;
+  }
+}
+
+const selectPrevious = (): void => {
+  if (suggestedTags.value.length > 0) {
+    const index = suggestedTags.value.findIndex(e => e.selected);
+    let previousIndex = 0;
+
+    if (index == -1 || index == 0) {
+      previousIndex = suggestedTags.value.length - 1;
+    } else {
+      previousIndex = index - 1;
+    }
+
+    if (index != -1) {
+      suggestedTags.value[index].selected = false;
+    }
+    suggestedTags.value[previousIndex].selected = true;
+  }
+}
+
+const sendSelected = (): void => {
+  const selected = suggestedTags.value.find(e => e.selected);
+  if (selected) {
+    emit("suggestionSelected", selected.title);
+  }
+}
+
+const onKeyDown = (keyCode: string):void => {
+  console.log("onKeyDown", keyCode);
+  switch (keyCode) {
+    case "ArrowDown":
+      selectNext();
+      break;
+    case "ArrowUp":
+      selectPrevious();
+      break;
+    case "Enter":
+      sendSelected();
+      break;
+  }
+}
+
+defineExpose({
+  onKeyDown
+});
 </script>
